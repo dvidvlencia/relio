4fe8003e4be0900d49e863634ae679d5e0a41ac3
Valencia <Valencia@Valencia-PC>
2016-03-25 00:36:30 -0600

    se termino una primera version para los efectos de desplazamiento izq & derecha

--- js/relio.js
+++ js/relio.js
@@ -4,49 +4,69 @@
         right: function (){
             $(this).each(function(){
                 var selector = $(this);
-                filterTag(selector, 'right');
+                var options = {
+                    valueDirection: '-100%'
+                };
+                filterTag(selector, options);
+                //for outside variable
+                //var filter = test();
+                //alert(filter);
+                
+                
             });
         },
         
         left: function (){
             $(this).each(function(){
                 var selector = $(this);
-                filterTag(selector, 'left');
+                var options = {
+                    valueDirection: '100%'
+                };
+                filterTag(selector, options);
             });
         }
-        
     });
 })(jQuery);
 /*
 debe captar el seletor y el nombre del efecto para poder ejecutar el transition
 */
-
-function filterTag(selector, effect){
-    console.log(effect);
+//function test(){
+//    variable = "outside variable";
+//    return variable;
+//}
+function filterTag(selector, options){
     var nameSelector = selector[0]['tagName'];
     if(nameSelector === 'A'){
+        var value = options.valueDirection;
         $("body").removeAttr("class");
-        /*capturamos el link destino*/
         var href = selector.attr('href');
         event.preventDefault();
-        $("body").addClass("testTagA");
-//        $("body").hide("slide", { direction: "left" }, 1000);
-//        alert(href);
+        $("body").animate(
+            { left : value},
+            {
+                easeing: 'easeInOutBounce',
+                duration: 500, 
+                complete: function(){
+                }
+            }
+        );
+        setTimeout(function(){ window.location.href = href; }, 500);
+        
     } else {
+        var value = options.valueDirection;
+        
         $("body").removeAttr("class");
         var href = selector.attr('data-link');
         $("body").animate(
-                {
-                    left: '-100%',
-                    opacity: '0.5'
-                },
-                {
-                    easeing: 'easeInOutBounce',
-                    duration: 900, 
-                    complete: function(){
-//                        alert('effect end, ' + href);
-                    }
+            { left : value},
+            {
+                easeing: 'easeInOutBounce',
+                duration: 500, 
+                complete: function(){
                 }
-            );
+            }
+        );
+        setTimeout(function(){ window.location.href = href; }, 500);
+        
     }
 }
